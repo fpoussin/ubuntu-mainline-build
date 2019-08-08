968f418462912e11b37bbbfe0afd3ac56727dead
Fabien Poussin <fabien.poussin@gmail.com>
2019-08-08 15:08:27 +0000

    Adding basic error checks

--- build.py
+++ build.py
@@ -19,7 +19,13 @@ if not os.path.exists(patch_folder):
   os.mkdir(patch_folder)
 
 def get_sources(ver):
-  sources = requests.get("https://kernel.ubuntu.com/~kernel-ppa/mainline/v{}/SOURCES".format(ver)).text.strip().split('\n')
+  r = requests.get("https://kernel.ubuntu.com/~kernel-ppa/mainline/v{}/SOURCES".format(ver))
+
+  if r.status_code != 200:
+    print("Kernel version not found")
+    raise NameError
+
+  sources = r.text.strip().split('\n')
   git = sources[0].split(' ')[0]
   patches = sources[1:]
 
@@ -41,27 +47,27 @@ def checkout_branch(ver):
   run("git -C {0} clean -fxd".format(linux_dir), shell=True)
   run("git -C {0} reset --hard".format(linux_dir, ver), shell=True)
   run("git -C {0} fetch --all --tags".format(linux_dir, ver), shell=True)
-  run("git -C {0} checkout v{1}".format(linux_dir, ver), shell=True)
+  run("git -C {0} checkout v{1}".format(linux_dir, ver), shell=True, check=True)
 
 def patch(version, patch):
   for r, d, f in os.walk(patch_folder):
     for file in sorted(f):
       print(file)
       with open(os.path.join(r, file), 'r') as data:
-        run("git apply ../{}".format(os.path.join(r, file)), cwd=linux_dir, shell=True)
+        run("git apply ../{}".format(os.path.join(r, file)), cwd=linux_dir, shell=True, check=True)
 
   for p in patch:
     p = p[0]
     print(p)
-    run("git apply ../{}".format(p), cwd=linux_dir, shell=True)
+    run("git apply ../{}".format(p), cwd=linux_dir, shell=True, check=True)
 
 def config():
   run("cp /boot/config-`uname -r` .config", cwd=linux_dir, shell=True)
-  run("nice make olddefconfig", cwd=linux_dir, shell=True, env=env)
-  run("scripts/config --disable DEBUG_INFO", cwd=linux_dir, shell=True, env=env)
+  run("nice make olddefconfig", cwd=linux_dir, shell=True, env=env, check=True)
+  run("scripts/config --disable DEBUG_INFO", cwd=linux_dir, shell=True, env=env, check=True)
 
 def build():
-  run("nice make -j `nproc` deb-pkg LOCALVERSION=-mainline", cwd=linux_dir, shell=True, env=env)
+  run("nice make -j `nproc` deb-pkg LOCALVERSION=-mainline", cwd=linux_dir, shell=True, env=env, check=True)
 
 get_sources(args.version)
 checkout_branch(args.version)
